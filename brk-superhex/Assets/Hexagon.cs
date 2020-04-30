d84e8385b1000d305caeb379fa43da936aac0346
slimjimadair <slimjimadair@gmail.com>
2020-04-30 22:02:14 +0100

    Basics Complete
    Followed video to get basic game functionality working in about 30mins.

---
+++ brk-superhex/Assets/Hexagon.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hexagon : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float shrinkSpeed = 3;
+
+    void Start()
+    {
+        rb.rotation = Random.Range(0f, 360f);
+        transform.localScale = Vector3.one * 15f;
+    }
+
+    void Update()
+    {
+        transform.localScale -= Vector3.one * shrinkSpeed * Time.deltaTime;
+        Debug.Log(rb.position);
+        if (transform.localScale.x <= 0.05f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ brk-superhex/Assets/Rotator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    public float rotateSpeed = 30f;
+
+    void Update()
+    {
+        transform.Rotate(Vector3.forward, Time.deltaTime * rotateSpeed);
+    }
+}
